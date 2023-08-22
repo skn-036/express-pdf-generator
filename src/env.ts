8f4cb0d73430900431f3579e7e8e7e7f40ab68e9
skn-036 <noman0703036@gmail.com>
2023-08-22 20:36:34 +0600

    #2

--- src/env.ts
+++ src/env.ts
@@ -3,12 +3,14 @@ dotenv.config();
 
 type Env = {
     appUrl: string;
+    port: string | number;
 };
 
 const envVars = process.env;
 
 const env: Env = {
     appUrl: envVars?.APP_URL ? envVars.APP_URL : '',
+    port: envVars?.PORT ? envVars?.PORT : 5000,
 };
 
 export default env;
